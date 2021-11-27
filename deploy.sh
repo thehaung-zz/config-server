38716ae981f103a852aa2444cd30177d25e45c81
Hau Nguyen <thehaung217@gmail.com>
2021-11-27 21:57:35 +0700

    build: change name container

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t registry.gitlab.com/3hvn/config-server
 docker stop config-server
 docker rm config-server
-docker run -d -p 8888:8888 --name api-gateway registry.gitlab.com/3hvn/config-server
+docker run -d -p 8888:8888 --name config-server registry.gitlab.com/3hvn/config-server
